6f5773f452229b7d2b4e19b4c338ffd7fd18eb36
m_khorosh <marina.khoroshilova@gmail.com>
2021-09-30 12:05:56 +0300

    init

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +12,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button bRun;
-    private static final int SHOW_OTHER_ACTIVITY = 1;
+    EditText eName;
 
+    public static final String key = "KEY_NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,68 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Toast.makeText(getApplicationContext(),"onCreate() called !",Toast.LENGTH_LONG).show();
         bRun = findViewById(R.id.bRun);
+        eName = findViewById(R.id.name);
         bRun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), OtherActivity.class);
-//                intent.putExtra(OtherActivity.GUESS,"Some guess");
-//                startActivityForResult(intent,SHOW_OTHER_ACTIVITY);
-
-//                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
-//                browserIntent.setData(Uri.parse("http://www.google.com"));
-//                browserIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:0123456789"));
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
-
-
-//                finish();
-
+                Intent intent = new Intent(getApplicationContext(), OtherActivity.class);
+                intent.putExtra(key, eName.getText().toString());
+                startActivityForResult(intent,0);
+                finish();
             }
         });
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode){
-            case (SHOW_OTHER_ACTIVITY):
-                if (resultCode == Activity.RESULT_OK){
-                    boolean isCorrect = data.getBooleanExtra(OtherActivity.IS_CORRECT, false);
-                    if (isCorrect){
-                        String str = getIntent().getStringExtra("KEY_NAME");
-                        Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
-                    }else{
-                        super.onActivityResult(requestCode, resultCode, data);
-                    }
-                }
-                break;
-        }
-    }
-
-
-//    protected void onStart() {
-//        super.onStart();
-//        Toast.makeText(getApplicationContext(),"onStart() called !",Toast.LENGTH_LONG).show();
-//    }
-//    protected void onPause() {
-//        super.onPause();
-//        Toast.makeText(getApplicationContext(),"onPause() called !",Toast.LENGTH_LONG).show();
-//    }
-//    protected void onRestart() {
-//        super.onRestart();
-//        Toast.makeText(getApplicationContext(),"onRestart() called !",Toast.LENGTH_LONG).show();
-//    }
-//    protected void onResume() {
-//        super.onResume();
-//        Toast.makeText(getApplicationContext(),"onResume() called !",Toast.LENGTH_LONG).show();
-//    }
-//    protected void onStop() {
-//        super.onStop();
-//        Toast.makeText(getApplicationContext(),"onStop() called !",Toast.LENGTH_LONG).show();
-//    }
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        Toast.makeText(getApplicationContext(),"onDestroy() called !",Toast.LENGTH_LONG).show();
-//    }
 }
--- app/src/main/java/com/example/helloandroid/OtherActivity.java
+++ app/src/main/java/com/example/helloandroid/OtherActivity.java
@@ -3,44 +3,16 @@ package com.example.helloandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OtherActivity extends AppCompatActivity {
-
-    EditText eName;
-    Button aRun;
-    public static final String key = "KEY_NAME";
-    private static final int SHOW_OTHER_ACTIVITY = 1;
-    public static final String IS_CORRECT = "com.example.helloandroid.MainActivity.iscorrect";
-    public static final String GUESS = "helloandroid.guess";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
-        eName = findViewById(R.id.name);
-        aRun = findViewById(R.id.aRun);
-        aRun.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra(key, eName.getText().toString());
-//                intent.putExtra(IS_CORRECT,isCorrect(guess));
-                setResult(RESULT_OK,intent);
-
-
-
-
-
-
-//                startActivityForResult(intent,SHOW_OTHER_ACTIVITY);
-                finish();
-            }});
+        String str = getIntent().getStringExtra("KEY_NAME");
+        Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
     }
-
 }
-
